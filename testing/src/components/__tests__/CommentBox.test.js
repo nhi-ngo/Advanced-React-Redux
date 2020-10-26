94b51e1cd7813c518949f36d11da9d2358c29755
nhi-ngo <nhingo.y@gmail.com>
2020-10-26 15:54:03 -0400

    more tests for CommentBox component

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -19,8 +19,14 @@ it('has a text area and a button', () => {
 
 it('has a text area that users can type in', () => {
   wrapper.find('textarea').simulate('change', { target: { value: 'new comment' } })
-
   wrapper.setProps({ value: 'new comment' })
-
   expect(wrapper.find('textarea').prop('value')).toEqual('new comment')
+})
+
+it('when form is submitted, text area gets emptied', () => {
+  wrapper.find('form').simulate('change', { target: { value: 'new comment' } })
+  wrapper.setProps({ value: 'new comment' })
+  wrapper.find('form').simulate('submit')
+  wrapper.setProps({ value: '' })
+  expect(wrapper.find('textarea').prop('value')).toEqual('')
 })
