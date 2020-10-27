69a2b5653ab9e132889153754264ed34e91eb604
nhi-ngo <nhingo.y@gmail.com>
2020-10-27 14:24:49 -0400

    describe statement

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -17,16 +17,19 @@ it('has a text area and a button', () => {
   expect(wrapper.find('button').length).toEqual(1)
 })
 
-it('has a text area that users can type in', () => {
-  wrapper.find('textarea').simulate('change', { target: { value: 'new comment' } })
-  wrapper.setProps({ value: 'new comment' })
-  expect(wrapper.find('textarea').prop('value')).toEqual('new comment')
-})
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapper.find('textarea').simulate('change', { target: { value: 'new comment' } })
+    wrapper.setProps({ value: 'new comment' })
+  })
 
-it('when form is submitted, text area gets emptied', () => {
-  wrapper.find('form').simulate('change', { target: { value: 'new comment' } })
-  wrapper.setProps({ value: 'new comment' })
-  wrapper.find('form').simulate('submit')
-  wrapper.setProps({ value: '' })
-  expect(wrapper.find('textarea').prop('value')).toEqual('')
+  it('has a text area that users can type in', () => {
+    expect(wrapper.find('textarea').prop('value')).toEqual('new comment')
+  })
+
+  it('when form is submitted, text area gets emptied', () => {
+    wrapper.find('form').simulate('submit')
+    wrapper.setProps({ value: '' })
+    expect(wrapper.find('textarea').prop('value')).toEqual('')
+  })
 })
